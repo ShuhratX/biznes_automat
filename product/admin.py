f625f985cde3c8fb273f97ff1915fa5f7b242018
ShuhratX <shuhratxayitov@gmail.com>
2021-06-26 10:44:19 +0500

    refactor

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'count')
+
+
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone')
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('summa', 'count')
+
+
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'salary')
+
+
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('summa', 'base')
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('summa', 'base')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Partner, PartnerAdmin)
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(Worker, WorkerAdmin)
+admin.site.register(Loan, LoanAdmin)
+admin.site.register(Expense, ExpenseAdmin)
